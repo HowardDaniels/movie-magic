057650d26a6945ec7fd490da75f7d35e0ba7af98
Shawn Yu <shawnyudesign@gmail.com>
2020-03-04 19:07:46 -0500

    members

--- public/js/members.js
+++ public/js/members.js
@@ -1,11 +1,23 @@
 
-var movieSearch= require("./movie-search");
+var db = require("../models");
+// var movieSearch= require("./movie-search");
 $(document).ready(function() {
   // GET requests and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    var myUserName=req.params.user_data;
-
-
-    $(".member-name").text(data.username);
+ $(".member-name").text(data.username);
   });
 });
+
+
+
+
+// db.User.findOne({
+//   where: {
+//     username: username
+//   }
+// }).then(function(dbUser) {
+//   // If there's no user with the given email
+//   if (!dbUser) {
+//     return done(null, false, {
+//       message: "Incorrect username."
+//     });
