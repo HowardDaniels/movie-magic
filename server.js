2ec20fab7088ee5597a027d4a16b94b24e0a18f6
Shawn Yu <shawnyudesign@gmail.com>
2020-03-05 09:44:55 -0500

    some minor changed in the html files

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require("express-session");
-
 var passport = require("./config/passport");
-
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 8080;
-
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,7 +16,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
 // Handlebars
 app.engine(
   "handlebars",
@@ -29,19 +24,15 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
 var syncOptions = { force: false };
-
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
-
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
@@ -52,5 +43,4 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
-
 module.exports = app;
